c0e86526c6242d23a2add83c6dedde8e3f71f84b
Trung Le Vu Tran <trungrueta@icloud.com>
2022-04-13 18:09:01 +0700

    write demo api

--- server/api/api.ts
+++ server/api/api.ts
@@ -1,3 +1,10 @@
-// export default createEventHandler(async () => {
-//   return { ok: true }
-// })
+// issue #4290
+// #intro alias not reference to correct definition file.
+// please uncomment custom definition in root project -> shim.dts to see result
+import { useRuntimeConfig } from "#nitro";
+
+const config = useRuntimeConfig();
+
+export default defineEventHandler(async () => {
+  return config;
+});
---
+++ shim.d.ts
@@ -0,0 +1,5 @@
+// issue #4290
+// fix nuxt 3 issue missing type hint in [#nitro]
+// declare module "#nitro" {
+//   export * from "nitropack/dist/runtime/index.d";
+// }
